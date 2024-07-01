3e78525b1e53154090f3984c398c0a86565644e2
narugo1992 <narugo99@gmail.com>
2024-07-01 22:26:47 +0800

    dev(narugo): beter query

--- maid_assistant/explain.py
+++ maid_assistant/explain.py
@@ -2,6 +2,7 @@ import io
 import json
 import logging
 import os
+import re
 from typing import Optional
 
 from hbutils.string import ordinalize
@@ -44,6 +45,44 @@ Description: <{lang} description>
     return response.choices[0].message.content.strip()
 
 
+def _get_desc_by_wiki_data(data, tag=None, use_other_names: bool = False):
+    with io.StringIO() as sf:
+        print(f'Tag: {tag or data["title"]}', file=sf)
+        if use_other_names and data['other_names']:
+            print(f'Other Names: {", ".join(data["other_names"])}', file=sf)
+        desc = data['body']
+        if desc:
+            print(f'Description: {desc}', file=sf)
+        else:
+            print('No description found.', file=sf)
+        return bool(desc), sf.getvalue()
+
+
+def _extract_wiki_titles(wiki_text):
+    pattern = r'\[\[(.*?)(?:\|(.*?))?\]\]'
+    links = re.findall(pattern, wiki_text)
+    extracted_wiki_titles = []
+    for link in links:
+        if '|' in link[0]:
+            page, text = link[0].split('|', 1)
+        else:
+            page, text = link[0], (link[1] or link[0])
+        extracted_wiki_titles.append(page.strip())
+
+    return extracted_wiki_titles
+
+
+def _get_wiki_info_by_title(title: str):
+    session = get_danbooru_session()
+    resp = srequest(session, 'GET', f'https://danbooru.donmai.us//wiki_pages.json',
+                    params={'search[title_normalize]': title}, raise_for_status=False)
+
+    if resp.json():
+        return resp.json()[0]
+    else:
+        return None
+
+
 def _get_desc(tag: str, use_other_names: bool = False):
     session = get_danbooru_session()
     resp = srequest(session, 'GET', f'https://danbooru.donmai.us/wiki_pages/{tag}.json', raise_for_status=False)
@@ -56,17 +95,39 @@ def _get_desc(tag: str, use_other_names: bool = False):
         except (JSONDecodeError, json.JSONDecodeError):
             return False, f'Tag: {tag}\nNo description found.'
 
-        with io.StringIO() as sf:
-            print(f'Tag: {tag}', file=sf)
-            if use_other_names and resp.json()['other_names']:
-                print(f'Other Names: {", ".join(resp.json()["other_names"])}', file=sf)
-            desc = resp.json()['body']
-            if desc:
-                print(f'Description: {desc}', file=sf)
-            else:
-                print('No description found.', file=sf)
+        title_attachments = []
+        if resp.json()['body']:
+            exist_related_tag_titles = {resp.json()['title']}
+            tag_titles = _extract_wiki_titles(resp.json()['body'])
+            for title in tag_titles:
+                title_data = _get_wiki_info_by_title(title)
+                if title_data and title_data['title'] not in exist_related_tag_titles:
+                    title_attachments.append((title, title_data))
+                    exist_related_tag_titles.add(title_data['title'])
 
-            return True, sf.getvalue()
+        with io.StringIO() as sf:
+            found, text = _get_desc_by_wiki_data(resp.json(), tag=tag, use_other_names=use_other_names)
+            print(f'## Tag {tag!r}', file=sf)
+            print(f'', file=sf)
+            print(text, file=sf)
+            print(f'', file=sf)
+
+            if title_attachments:
+                print(f'## Mentioned Tags Information', file=sf)
+                print(f'', file=sf)
+                print(f'The following parts are attachment tag '
+                      f'information mentioned in tag {tag!r}s description body.', file=sf)
+                print(f'', file=sf)
+                for title, title_data in title_attachments:
+                    title_found, title_text = _get_desc_by_wiki_data(title_data, tag=title,
+                                                                     use_other_names=use_other_names)
+                    if title_found:
+                        print(f'### {title}', file=sf)
+                        print(f'', file=sf)
+                        print(title_text, file=sf)
+                        print(f'', file=sf)
+
+            return found, sf.getvalue()
 
 
 def _raw_explain(tag: str, lang: str = 'english', use_other_names: bool = False):
